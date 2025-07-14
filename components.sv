87a7aca469f68adc3372f5c4d3ec979ca2665aec
Intro <intro-iu@outlook.com>
2025-07-14 23:59:41 +0800

    feat(comp) Data Extenting

--- components.sv
+++ components.sv
@@ -9,7 +9,7 @@
 |   4'b0100	        XOR (按位异或)	            Result = A ^ B		                        |
 |   4'b0101	        SLL (逻辑左移)	            Result = A << B[4:0]		                |
 |   4'b0110	        SRL (逻辑右移)	            Result = A >> B[4:0]		                |
-|   4'b0111	        SRA (算数右移)	            Result = $signed(A) >>> B[4:0]	            |	
+|   4'b0111	        SRA (算数右移)	            Result = $signed(A) >>> B[4:0]	            |
 |   4'b1000	        SLT (有符号小于则置位)      Result = ($signed(A) < $signed(B))          |
 |   4'b1001	        SLTU(无符号小于则置位)	    Result = (A < B)                            |
 +-------------------------------------------------------------------------------------------+
@@ -19,19 +19,17 @@
 `include "defines.svh"
 
 module ALU (
-    input [31:0] A,
-    input [31:0] B,
-    input [ 3:0] ALUOp,
+    input  logic [31:0] A,
+    input  logic [31:0] B,
+    input  logic [ 3:0] ALUOp,
 
     output logic [31:0] Result,       // 结果
     output logic        Zero,         // 为零
     output logic        Carry,        // 进位
     output logic        Overflow      // 溢出
 );
-
     // ALU操作
     always_comb begin
-
         // 默认值
         Result   = 'x;
         Carry    = '0;
@@ -56,11 +54,16 @@ module ALU (
             `ALU_SLT:   Result = ($signed(A) < $signed(B)) ? '1 : '0;
             `ALU_SLTU:  Result = (A < B) ? '1 : '0;
         endcase
-
         Zero = Result == 32'b0 ? '1 : '0;
-
     end
-
-    
 endmodule
 
+module dataExt #(
+    parameter int INPUT_WIDTH = 12,
+    parameter bit extType = 1,
+)(
+    input [INPUT_WIDTH-1:0] data,
+    output logic [31:0] ext
+);
+    assign ext = extType ? signed'(data) : unsigned'(data);
+endmodule
